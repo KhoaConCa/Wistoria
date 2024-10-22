21af987a1cb9444295ba79ccf4b398401530ed1f
KhoaDN <dnk270204@gmail.com>
2024-10-22 19:54:09 +0700

    done get data form mongo to campusD

---
+++ Assets/Projects/Scripts/Utilities/BaseHandler.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHandler
+{
+    /// <summary>
+    /// Rebuild FromJson function to be able with array JSON
+    /// </summary>
+    /// <typeparam name="T">Any Data file</typeparam>
+    /// <param name="json">JSON as string</param>
+    /// <returns>Array of data</returns>
+    public static T[] FromJson<T>(string json)
+    {
+        // Wrap the JSON array in a root object called "array"
+        string newJson = $"{{\"array\":{json}}}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+    [Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
